c5128ba47edd8778fd5d284efc695258a87ef71d
Destiny <desto4q@gmail.com>
2023-10-04 02:04:23 +0100

    fixed

--- src/components/Video_details.jsx
+++ src/components/Video_details.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Video_details() {
   return (
-    <div className="video_details"></div>
+    <div className="video_details">
+        <img src="" alt="" />
+    </div>
   )
 }
 
